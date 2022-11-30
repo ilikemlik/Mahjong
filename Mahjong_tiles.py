00612c2645b4bf170c429a90101e60b3e55612e3
ilikemlik <jonathanoei04@gmail.com>
2022-11-30 14:41:50 +0800

    Adding wash() and player object

--- Mahjong_tiles.py
+++ Mahjong_tiles.py
@@ -1,4 +1,4 @@
-print("Hello World")
+
 import random
 
 class tile(object):
@@ -13,6 +13,7 @@ class tile(object):
 class tileWall(object):
     def __init__(self):
         self.tiles = []
+        self.discardPile = []
         self.build()
     
     def build(self):
@@ -33,15 +34,44 @@ class tileWall(object):
             for num in range(1,5):
                 self.tiles.append(tile(flower,num))
 
+    def wash(self):
+        for i in range(len(self.tiles)-1,0,-1):
+            rand = random.randint(0, i)
+            #wash by swapping two tiles to make a random tilewall
+            self.tiles[i], self.tiles[rand] = self.tiles[rand], self.tiles[i] 
+
     def show(self):
         for t in self.tiles:
             t.show()
-        print("length is :", len(self.tiles))
+        # print("length is :", len(self.tiles))
+    
+    def drawTile(self):
+        return self.tiles.pop()
 
 class Player(object):
-    def __init__(self):
-        pass
+    def __init__(self,name):
+        self.name = name
+        self.hand = []
+
+    def draw(self, tileWall):
+        self.hand.append(tileWall.drawTile())
+        return self
+
+    def showHand(self):
+        for tile in self.hand:
+            tile.show()
+
+    def discard(self, tileWall):
+        tileWall.discardPile.append(self.hand[-1])
+        return self.hand.pop()
+
 
 # FirstTile = tile("bamboo",3)
 # FirstTile.show()
-tileWall().show()
+Tiles = tileWall()
+Tiles.wash()
+Tiles.show()
+# P1 = Player("player1")
+# P1.draw(Tiles)
+# P1.draw(Tiles)
+# P1.showHand()
